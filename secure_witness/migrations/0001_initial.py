99300bb90311f066bb08e6c695ead7911c5adb1d
sdgennari <sdgennari@gmail.com>
2015-04-01 10:50:20 -0400

    Fixed local database and migration issues

--- secure_witness/migrations/0001_initial.py
+++ secure_witness/migrations/0001_initial.py
@@ -2,25 +2,42 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+from django.conf import settings
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.CreateModel(
+            name='File',
+            fields=[
+                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
+                ('file_name', models.CharField(max_length=255)),
+                ('file_id', models.CharField(max_length=255)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
             name='Folder',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
                 ('folder_name', models.CharField(max_length=255)),
                 ('folder_id', models.CharField(max_length=255)),
-                ],),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
         migrations.CreateModel(
             name='Keyword',
             fields=[
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', primary_key=True, serialize=False)),
+                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
                 ('word', models.CharField(max_length=50)),
             ],
             options={
@@ -30,17 +47,33 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Report',
             fields=[
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', primary_key=True, serialize=False)),
+                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
                 ('time', models.DateTimeField(auto_now=True)),
                 ('short', models.CharField(max_length=200)),
                 ('detailed', models.CharField(max_length=1000)),
                 ('location', models.CharField(max_length=50)),
                 ('date', models.CharField(max_length=1000)),
-                ('keywords', models.CharField(max_length=1000)),
                 ('private', models.BooleanField(default=False)),
+                ('keywords', models.ManyToManyField(to='secure_witness.Keyword')),
             ],
             options={
             },
             bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='UserProfile',
+            fields=[
+                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
+                ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='file',
+            name='folder',
+            field=models.ForeignKey(to='secure_witness.Folder'),
+            preserve_default=True,
         ),
     ]
