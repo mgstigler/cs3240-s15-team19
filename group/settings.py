2cc7da2eb8edad170ac61f2320d92c1f9d823d89
sdgennari <sdgennari@gmail.com>
2015-04-26 12:06:18 -0400

    Updated settings.py. Created new migrations

--- group/settings.py
+++ group/settings.py
@@ -110,7 +110,12 @@ TEMPLATE_DIRS = (
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+
+# ------------------------------------------------------------
+# NOTE: Commenting this line makes the app work locally.
+# This needs to be uncommented before uploading to Heroku
+# ------------------------------------------------------------
+# DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
