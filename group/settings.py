b961ff316496589d3ab2f6ae30b07b9c9edad7aa
sdgennari <sdgennari@gmail.com>
2015-04-27 02:50:21 -0400

    Updated files to prep for Heroku

--- group/settings.py
+++ group/settings.py
@@ -121,13 +121,13 @@ EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'g19securewitness@gmail.com'
 
 # Parse database configuration from $DATABASE_URL
-#import dj_database_url
+import dj_database_url
 
 # ------------------------------------------------------------
 # NOTE: Commenting this line makes the app work locally.
 # This needs to be uncommented before uploading to Heroku
 # ------------------------------------------------------------
-# DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
