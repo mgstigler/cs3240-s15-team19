b9f1f712a86116bd6da33e5b34c8128ef0b5d23f
mgs4ff <mgs4ff@virginia.edu>
2015-04-27 15:30:56 -0400

    heroku commit

--- group/settings.py
+++ group/settings.py
@@ -14,6 +14,7 @@ import os.path
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
@@ -121,13 +122,13 @@ EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'g19securewitness@gmail.com'
 
 # Parse database configuration from $DATABASE_URL
-#import dj_database_url
+import dj_database_url
 
 # ------------------------------------------------------------
 # NOTE: Commenting this line makes the app work locally.
 # This needs to be uncommented before uploading to Heroku
 # ------------------------------------------------------------
-# DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
